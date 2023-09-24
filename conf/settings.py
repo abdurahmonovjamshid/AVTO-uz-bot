a84e94f83d43ae8d99302f3d4f112d01e883a92d
abdurahmonovjamshid <abdurahmonovjamshid060@gmail.com>
2023-09-24 20:36:26 +0500

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'o4jOV2QOAH3delVms1hR',
+        'HOST': 'containers-us-west-162.railway.app',
+        'PORT': '7985',
     }
 }
 
@@ -134,8 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_URLS = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
