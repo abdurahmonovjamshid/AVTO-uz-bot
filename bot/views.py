7644a32ec661078cb061e11c718b4c41a661cc38
abdurahmonovjamshid <abdurahmonovjamshid060@gmail.com>
2024-03-02 10:06:36 +0500

    limited description

--- bot/views.py
+++ bot/views.py
@@ -270,7 +270,7 @@ def cm_start(message):
             cars = user.car_set.filter(complate=True)
             for car in cars:
                 try:
-                    text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\nQo'shimcha malumot: \n{car.description[:900]},\n\nBog'lanish: {car.contact_number}\n\n"
+                    text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\nQo'shimcha malumot: \n{car.description[:800]},\n\nBog'lanish: {car.contact_number}\n\n"
                     text += f"👁: {car.seen.count()}, "
                     text += f"👍: {car.likes.count()}, "
                     text += f"👎: {car.dislikes.count()}\n\n"
@@ -400,7 +400,7 @@ def remove_message(call):
                 bot.send_message(chat_id=car.owner.telegram_id,
                                  text=f"<b>{car}</b> e'loningiz faollashtirildi!", parse_mode='html')
 
-                text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\nQo'shimcha malumot: \n{car.description[:900]},\n\nBog'lanish: {car.contact_number}"
+                text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\nQo'shimcha malumot: \n{car.description[:800]},\n\nBog'lanish: {car.contact_number}"
                 media_group = [telebot.types.InputMediaPhoto(
                     media=car.images.first().image_link, caption=text)]
                 for photo in car.images.all()[1:]:
@@ -429,7 +429,7 @@ def retrieve_car(call):
         car.seen.add(TgUser.objects.get(telegram_id=call.from_user.id))
 
         text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\n"
-        text += f"Qo'shimcha malumot: \n{car.description[:900]},\n\n"
+        text += f"Qo'shimcha malumot: \n{car.description[:800]},\n\n"
         text += f"Bog'lanish: {car.contact_number}\n\n"
         text += f"👁: {car.seen.count()}, "
         text += f"👍: {car.likes.count()}, "
@@ -469,7 +469,7 @@ def retrieve_car(call):
 
         reply_to_message = call.message.reply_to_message
         text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\n"
-        text += f"Qo'shimcha malumot: \n{car.description[:900]},\n\n"
+        text += f"Qo'shimcha malumot: \n{car.description[:800]},\n\n"
         text += f"Bog'lanish: {car.contact_number}\n\n"
         text += f"👁: {car.seen.count()}, "
         text += f"👍: {car.likes.count()}, "
@@ -499,7 +499,7 @@ def retrieve_car(call):
 
         reply_to_message = call.message.reply_to_message
         text = f"Nomi: {car.name},\nModeli: {car.model},\nIshlab chiqarilgan yil: {car.year},\nNarxi: {'{:,.2f}'.format(car.price).rstrip('0').rstrip('.')}$,\n"
-        text += f"Qo'shimcha malumot: \n{car.description[:900]},\n\n"
+        text += f"Qo'shimcha malumot: \n{car.description[:800]},\n\n"
         text += f"Bog'lanish: {car.contact_number}\n\n"
         text += f"👁: {car.seen.count()}, "
         text += f"👍: {car.likes.count()}, "
